b2245455c4a208ea6bfb64907188297bc4c49411
Beatles9527 <838890974@qq.com>
2021-08-19 22:03:47 +0800

    mybatis快速应用

--- Lg-java-practice/mybatis/mybatis_quickerStarter/src/main/java/cn/redblood/test/MybatisTest.java
+++ Lg-java-practice/mybatis/mybatis_quickerStarter/src/main/java/cn/redblood/test/MybatisTest.java
@@ -25,6 +25,7 @@ public class MybatisTest {
         // 2.构造sql工厂
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
         // 3.开启sqlSession
+        // 默认开启一个事务，但是该事务不会自动自动提交，在进行增删改查是，需要手动提交。。
         SqlSession sqlSession = sqlSessionFactory.openSession();
         // 4.执行sql
         List<User> users = sqlSession.selectList("user.findAll");
@@ -41,17 +42,17 @@ public class MybatisTest {
         InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
         // 2.构造sql工厂
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-        // 3.开启sqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        // 3.开启sqlSession(当openSession入参设置为true时，事务自动提交！)
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
         // 4.设置参数
         User user = new User();
-        user.setId(2);
-        user.setUsername("田金东");
+        user.setId(4);
+        user.setUsername("田东");
         // 5.插入数据
         sqlSession.insert("user.saveUser", user);
         // TODO: 2021/8/7 在做增删改的操作时，需要提交事务！！！
         // 6.提交事务
-        sqlSession.commit();
+//        sqlSession.commit();
 
         // 5.关闭会话
         sqlSession.close();
