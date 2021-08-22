4bba8c8d91e7b01125041d344643570f2b443da2
Beatles9527 <838890974@qq.com>
2021-08-22 11:17:44 +0800

    mybatis快速应用

--- Lg-java-practice/mybatis/mybatis_quickerStarter/src/main/java/cn/redblood/test/MybatisTest.java
+++ Lg-java-practice/mybatis/mybatis_quickerStarter/src/main/java/cn/redblood/test/MybatisTest.java
@@ -29,7 +29,7 @@ public class MybatisTest {
         // 默认开启一个事务，但是该事务不会自动自动提交，在进行增删改查是，需要手动提交。。
         SqlSession sqlSession = sqlSessionFactory.openSession();
         // 4.执行sql
-        List<User> users = sqlSession.selectList("user.findAll");
+        List<User> users = sqlSession.selectList("cn.redblood.dao.IUserDao.findAll");
         for (User user : users) {
             System.out.println(user);
         }
@@ -47,10 +47,10 @@ public class MybatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
         // 4.设置参数
         User user = new User();
-        user.setId(4);
-        user.setUsername("田东");
+        user.setId(3);
+        user.setUsername("田金东");
         // 5.插入数据
-        sqlSession.insert("user.saveUser", user);
+        sqlSession.insert("cn.redblood.dao.IUserDao.saveUser", user);
         // TODO: 2021/8/7 在做增删改的操作时，需要提交事务！！！
         // 6.提交事务
 //        sqlSession.commit();
